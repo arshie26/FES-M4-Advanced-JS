8c0406b73ca9ce01a278322af0f80ba131b7219e
arshie26 <arshtist@gmail.com>
2025-12-26 21:41:11 -0500

    Responsive

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function moveBackground(event){
+    const shapes = document.querySelectorAll(".shape");
+    const scaleFactor = 1/20;
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+    console.log(x, y);
+
+    for(let i = 0; i < shapes.length; ++i){
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd? -1:1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
+
 function contact(event){
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading');
